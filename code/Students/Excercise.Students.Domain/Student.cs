e30e4eba67b961f6467d0f4ab6c77f4ff73a088d
Ariel <ariel.lenisd@gmail.com>
2018-05-13 21:33:03 -0400

    Create domain project

---
+++ code/Students/Excercise.Students.Domain/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise.Students.Domain
+{
+    public class Student
+    {
+        public string Type { get; set; }
+        public string Name { get; set;}
+        public EGender Gender { get; set; }
+        public DateTime LastModification { get; set; }
+    }
+}
