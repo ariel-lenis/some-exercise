c54f2045443c59c1172a1f6c827e4eddcb71e956
Ariel <ariel.lenisd@gmail.com>
2018-05-13 22:43:44 -0400

    Parse raw arguments

---
+++ code/Students/Excercise.Students.Controllers.ConsoleController/Arguments.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise.Students.Controllers.ConsoleController
+{
+    public class Arguments
+    {
+        public string FileName { get; set; }
+        public Dictionary<string, string> Queries { get; set; }
+
+        public Arguments()
+        {
+            this.Queries = new Dictionary<string, string>();
+        }
+    }
+}
--- code/Students/Excercise.Students.Controllers.ConsoleController/ConsoleController.cs
+++ code/Students/Excercise.Students.Controllers.ConsoleController/ConsoleController.cs
@@ -2,6 +2,7 @@
 using LoggerProvider;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,70 @@ namespace Excercise.Students.Controllers.ConsoleController
     {
         public void Start(string[] arguments)
         {
-            StudentsLogger.Instance.Info("Starting console controller!");
+            try
+            {
+                StudentsLogger.Instance.Info("Starting console controller.");
+
+                Arguments parsedArguments = ParseArguments(arguments);
+                ProcessArguments(parsedArguments);
+
+                StudentsLogger.Instance.Info("Console controller has finished.");
+            }
+            catch (Exception error)
+            {
+                StudentsLogger.Instance.Error("Error at console controller layer", error);
+                throw;
+            }
+        }
+
+        private void ProcessArguments(Arguments parsedArguments)
+        {
+            if (!File.Exists(parsedArguments.FileName))
+            {
+                throw new Exception("The target CSV file doesn't exists.");
+            }
+
+
+        }
+
+        private Arguments ParseArguments(string[] arguments)
+        {
+            if (arguments.Length == 0)
+            {
+                throw new Exception("There must be at least one argument.");
+            }
+
+            Arguments result = new Arguments();
+            result.FileName = arguments[0];
+
+            for (int i = 1; i < arguments.Length; i++)
+            {
+                ParseQuery(arguments[i], result);
+            }
+
+            return result;
+        }
+
+        private void ParseQuery(string rawQuery, Arguments result)
+        {
+            string[] parts = rawQuery.Split('=');
+
+            if (parts.Length != 2)
+            {
+                throw new Exception("Bad format in queries arguments.");
+            }
+
+            string key = parts[0];
+            string value = parts[1];
+
+            if (result.Queries.ContainsKey(key))
+            {
+                throw new Exception("Duplicated Query.");
+            }
+
+            StudentsLogger.Instance.Info($"Parsed Query Key: [{key}], Value: [{value}]");
+
+            result.Queries.Add(key, value);
         }
     }
 }
